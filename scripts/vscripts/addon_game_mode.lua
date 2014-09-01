8a87f01463ef0fc6c14a040b0fff68eb989db51a
Mad Byte <alexfaisst@gmail.com>
2014-09-01 11:38:36 +0200

    Enabled AllRandom, Disable Deathmatch
    Enabled AllRandom
    Disabled Deatchmatch, because it crash the game

--- scripts/vscripts/addon_game_mode.lua
+++ scripts/vscripts/addon_game_mode.lua
@@ -11,8 +11,8 @@ end
 -- ACTIVATE
 --------------------------------------------------------------------------------
 function Activate()
-    GameRules.DotaPvP = DotaPvP()
-    GameRules.DotaPvP:InitGameMode()
+	GameRules.DotaPvP = DotaPvP()
+	GameRules.DotaPvP:InitGameMode()
 end
 
 function Precache( context )
@@ -131,9 +131,10 @@ function Precache( context )
 	PrecacheUnitByNameSync("npc_dota_hero_wisp", context)
 	PrecacheUnitByNameSync("npc_dota_hero_witch_doctor", context)
 	PrecacheUnitByNameSync("npc_dota_hero_zuus", context)
-    print('Done precaching!')
+	print('Done precaching!')
 end
 
+-- disable 'wtf-mode' by default
 local wtf_mode = 0
 
 --------------------------------------------------------------------------------
@@ -145,12 +146,13 @@ function DotaPvP:InitGameMode()
 	-- Enable the standard Dota PvP game rules
 	GameRules:GetGameModeEntity():SetTowerBackdoorProtectionEnabled( true )
 	GameRules:SetSameHeroSelectionEnabled( true )
+	GameRules:SetHeroSelectionTime( 0.0 )
 
 	-- Register Think
 	GameMode:SetContextThink( "DotaPvP:GameThink", function() return self:GameThink() end, 0.25 )
 
 	-- Register Game Events
-    ListenToGameEvent('player_connect_full', Dynamic_Wrap(DotaPvP, 'OnConnectFull'), self)
+	ListenToGameEvent('player_connect_full', Dynamic_Wrap(DotaPvP, 'OnConnectFull'), self)
 	ListenToGameEvent('npc_spawned', Dynamic_Wrap(DotaPvP, 'OnNPCSpawned'), self)
 	ListenToGameEvent('entity_killed', Dynamic_Wrap(DotaPvP, 'OnEntityKilled'), self)
 	ListenToGameEvent('dota_player_used_ability', Dynamic_Wrap(DotaPvP, 'OnAbilityUsed'), self)
@@ -158,95 +160,109 @@ function DotaPvP:InitGameMode()
 	 -- Register Commands
 	Convars:RegisterCommand( "wtf", Dynamic_Wrap(DotaPvP, 'ToggleWTFMode'), "A console command to toggle the wtf mode", 0 )
 	
-    -- userID map
-    self.vUserIDMap = {}
-    self.nLowestUserID = 2
+	-- userID map
+	self.vUserIDMap = {}
+	self.nLowestUserID = 2
 
-    -- Change random seed
-    local seed = string.gsub(string.gsub(GetSystemTime(), ':', ''), '0','')
-    math.randomseed(tonumber(seed))
+	-- Change random seed
+	local seed = string.gsub(string.gsub(GetSystemTime(), ':', ''), '0','')
+	math.randomseed(tonumber(seed))
 
-    -- Load ability List
-    self:LoadAbilityList()
+	-- Load ability List
+	self:LoadAbilityList()
+	
+	-- list of players with heroes
+	self.hasHero = {}
 			
 	print( "Random OMG loaded." )
 end
 
 --------------------------------------------------------------------------------
 function DotaPvP:GameThink()
+	for _,ply in pairs( self.vUserIDMap ) do
+		local playerID = ply:GetPlayerID()
+		if PlayerResource:IsValidPlayerID(playerID) then	
+			if not self.hasHero[playerID] then		
+				print('Try to pick a hero for user:', playerID)
+				CreateHeroForPlayer(self:ChooseRandomHero(),ply)
+				self.hasHero[playerID] = true
+				print('Hero picked for user:', playerID)
+			end
+		end
+	end
 	return 0.25
 end
 
 function DotaPvP:LoadAbilityList()
-    local abs = LoadKeyValues("scripts/kv/abilities.kv")
-    self.heroListKV = LoadKeyValues("scripts/npc/npc_heroes.txt")
-    self.subAbilities = LoadKeyValues("scripts/kv/abilityDeps.kv")
+	local abs = LoadKeyValues("scripts/kv/abilities.kv")
+	self.heroListKV = LoadKeyValues("scripts/npc/npc_heroes.txt")
+	self.subAbilities = LoadKeyValues("scripts/kv/abilityDeps.kv")
 
-    -- Build list of heroes
-    self.heroList = {}
-    self.heroListEnabled = {}
-    for heroName, values in pairs(self.heroListKV) do
-        -- Validate hero name
-        if heroName ~= 'Version' and heroName ~= 'npc_dota_hero_base' then
-            -- Make sure the hero is enabled
-            if values.Enabled == 1 then
-                -- Store this unit
-                table.insert(self.heroList, heroName)
-                self.heroListEnabled[heroName] = 1
-            end
-        end
-    end
+	-- Build list of heroes
+	self.heroList = {}
+	self.heroListEnabled = {}
+	for heroName, values in pairs(self.heroListKV) do
+		-- Validate hero name
+		if heroName ~= 'Version' and heroName ~= 'npc_dota_hero_base' then
+			-- Make sure the hero is enabled
+			if values.Enabled == 1 then
+				-- Store this unit
+				table.insert(self.heroList, heroName)
+				self.heroListEnabled[heroName] = 1
+			end
+		end
+	end
 
-    -- Table containing every skill
-    self.vAbList = {}
-    self.vAbListSort = {}
-    self.vAbListLookup = {}
+	-- Table containing every skill
+	self.vAbList = {}
+	self.vAbListSort = {}
+	self.vAbListLookup = {}
 
-    -- Build skill list
-    for k,v in pairs(abs) do
-        for kk, vv in pairs(v) do
-            -- This comparison is really dodgy for some reason
-            if tonumber(vv) == 1 then
-                -- Attempt to find the owning hero of this ability
-                local heroOwner = self:FindHeroOwner(kk)
+	-- Build skill list
+	for k,v in pairs(abs) do
+		for kk, vv in pairs(v) do
+			-- This comparison is really dodgy for some reason
+			if tonumber(vv) == 1 then
+				-- Attempt to find the owning hero of this ability
+				local heroOwner = self:FindHeroOwner(kk)
 
-                -- Store this skill
-                table.insert(self.vAbList, {
-                    name = kk,
-                    sort = k,
-                    hero = heroOwner
-                })
+				-- Store this skill
+				table.insert(self.vAbList, {
+					name = kk,
+					sort = k,
+					hero = heroOwner
+				})
 
-                -- Store into the sort container
-                if not self.vAbListSort[k] then
-                    self.vAbListSort[k] = {}
-                end
+				-- Store into the sort container
+				if not self.vAbListSort[k] then
+					self.vAbListSort[k] = {}
+				end
 
-                -- Store the sort reference
-                table.insert(self.vAbListSort[k], kk)
+				-- Store the sort reference
+				table.insert(self.vAbListSort[k], kk)
 
-                -- Store the reverse lookup
-                self.vAbListLookup[kk] = k
-            end
-        end
-    end
+				-- Store the reverse lookup
+				self.vAbListLookup[kk] = k
+			end
+		end
+	end
 end
 
 function DotaPvP:FindHeroOwner(skillName)
-    local heroOwner = ""
-    for heroName, values in pairs(self.heroListKV) do
-        if type(values) == "table" then
-            for i = 1, 16 do
-                if values["Ability"..i] == skillName then
-                    heroOwner = heroName
-                    goto foundHeroName
-                end
-            end
-        end
-    end
+	local heroOwner = ""
+	for heroName, values in pairs(self.heroListKV) do
+		if type(values) == "table" then
+			for i = 1, 16 do
+				if values["Ability"..i] == skillName then
+					heroOwner = heroName
+					goto foundHeroName
+				end
+			end
+		end
+	end
 
-    ::foundHeroName::
-    return heroOwner
+	::foundHeroName::
+	return heroOwner
 end
 
 -- This is an example console command
@@ -271,24 +287,25 @@ function DotaPvP:ToggleWTFMode()
 end
 
 function DotaPvP:OnConnectFull(keys)
-    -- Grab the entity index of this player
-    local entIndex = keys.index+1
-    local ply = EntIndexToHScript(entIndex)
+	-- Grab the entity index of this player
+	local entIndex = keys.index+1
+	local ply = EntIndexToHScript(entIndex)
 
-    local playerID = ply:GetPlayerID()
-	
-    -- Store into our map
-    self.vUserIDMap[keys.userid] = ply
-    self.nLowestUserID = self.nLowestUserID + 1
+	local playerID = ply:GetPlayerID()
+
+	-- Store into our map
+	self.vUserIDMap[keys.userid] = ply
+	self.nLowestUserID = self.nLowestUserID + 1
 end
 
 function DotaPvP:OnNPCSpawned(keys)
-    local unit = EntIndexToHScript( keys.entindex )
-	
-    if unit and unit:IsRealHero() then
+	local unit = EntIndexToHScript( keys.entindex )
+
+	if unit and unit:IsRealHero() then
 		local playerID = unit:GetPlayerOwnerID()
-		if self:IsValidPlayerID(playerID) then
-			local hero = self:ChangeHero(unit,self:ChooseRandomHero())
+		if PlayerResource:IsValidPlayerID(playerID) then
+			local hero = nil
+			--hero = self:ChangeHero(unit,self:ChooseRandomHero())
 			-- Change skills
 			if hero == nil then
 				self:ApplyBuild(unit, {
@@ -297,6 +314,7 @@ function DotaPvP:OnNPCSpawned(keys)
 					[3] = self:GetRandomAbility(),
 					[4] = self:GetRandomAbility('Ults')
 				})
+				unit:SetAbilityPoints(unit:GetLevel())
 			else
 				self:ApplyBuild(hero, {
 					[1] = self:GetRandomAbility(),
@@ -310,209 +328,180 @@ function DotaPvP:OnNPCSpawned(keys)
 end
 
 function DotaPvP:OnAbilityUsed(keys)
-    local playerID = EntIndexToHScript( keys.PlayerID )
-	
 	if wtf_mode == 1 then
-		if self:IsValidPlayerID(playerID) then
-			DotaPvP:RefreshAllSkills(PlayerResource:GetPlayer(playerID):GetAssignedHero())
+		for _,ply in pairs( self.vUserIDMap ) do
+			DotaPvP:RefreshAllSkills(ply:GetAssignedHero())	
 		end
 	end
 end
 
-function DotaPvP:OnEntityKilled(keys)	
+function DotaPvP:OnEntityKilled(keys)
 	local killedUnit = EntIndexToHScript( keys.entindex_killed )
     local killerEntity = nil
 
-    if keys.entindex_attacker ~= nil then
-        killerEntity = EntIndexToHScript( keys.entindex_attacker )
-    end
+	if keys.entindex_attacker ~= nil then
+		killerEntity = EntIndexToHScript( keys.entindex_attacker )
+	end
 	
-    if killedUnit and killedUnit:IsRealHero() then		
+	if killedUnit and killedUnit:IsRealHero() then
 		--TODO Check how to enable 'fast respawn'
 		--print('GetTimeUntilRespawn()', killedUnit:GetTimeUntilRespawn())
 		--killedUnit:SetTimeUntilRespawn(killedUnit:GetTimeUntilRespawn()/2)
 		--print('GetTimeUntilRespawn()', killedUnit:GetTimeUntilRespawn())
-    end
-end
-
--- Loops over all players, return true to stop the loop
-function DotaPvP:LoopOverPlayers(callback)
-    for k, v in pairs(self.vUserIDMap) do
-        -- Validate the player
-        if IsValidEntity(v) then
-            -- Run the callback
-            if callback(v, v:GetPlayerID()) then
-                break
-            end
-        end
-    end
-end
-
-function DotaPvP:IsValidPlayerID(checkPlayerID)
-    local isValid = false
-    self:LoopOverPlayers(function(ply, playerID)
-        if playerID == checkPlayerID then
-            isValid = true
-            return true
-        end
-    end)
-
-    return isValid
-end
-
-function DotaPvP:GetPlayerList()
-    local plyList = {}
-
-    self:LoopOverPlayers(function(ply, playerID)
-        table.insert(plyList, ply)
-    end)
-
-    return plyList
+	end
 end
 
 function DotaPvP:ApplyBuild(hero, build)
-    if hero == nil then
+	if hero == nil then
 		if build == nil then
 			Say(nil, COLOR_RED..'WARNING: Failed to apply a build!', false)
 			return
 		end
-    end
+	end
 
-    -- Grab playerID
-    local playerID = hero:GetPlayerID()
-    if not self:IsValidPlayerID(playerID) then
-        return
-    end
+	-- Grab playerID
+	local playerID = hero:GetPlayerID()
+	if not PlayerResource:IsValidPlayerID(playerID) then
+		return
+	end
 
-    -- Remove all the skills from our hero
-    self:RemoveAllSkills(hero)
+	-- Remove all the skills from our hero
+	self:RemoveAllSkills(hero)
 
-    -- Table to store all the extra skills we need to give
-    local extraSkills = {}
+	-- Table to store all the extra skills we need to give
+	local extraSkills = {}
 
-    -- Give all the abilities in this build
-    for k,v in ipairs(build) do
-        -- Check if this skill has sub abilities
-        if self.subAbilities[v] then
-            -- Store that we need this skill
-            extraSkills[self.subAbilities[v]] = true
+	-- Give all the abilities in this build
+	for k,v in ipairs(build) do
+		-- Check if this skill has sub abilities
+		if self.subAbilities[v] then
+			-- Store that we need this skill
+			extraSkills[self.subAbilities[v]] = true
 			-- TODO: Check if there is a better way to do this
 			-- Check if this skill has sub abilities too
 			if self.subAbilities[self.subAbilities[v]] then
 				-- Store that we need this skill
 				extraSkills[self.subAbilities[self.subAbilities[v]]] = true
 			end
-        end
+		end
 
-        -- Add to build
-        hero:AddAbility(v)
-    end
+		-- Add to build
+		hero:AddAbility(v)
+	end
 
-    -- Add missing abilities
-    local i = #build+1
-    for k,v in pairs(extraSkills) do
-        -- Add the ability
-        hero:AddAbility(k)
+	-- Add missing abilities
+	local i = #build+1
+	for k,v in pairs(extraSkills) do
+		-- Add the ability
+		hero:AddAbility(k)
 
-        -- Move onto the next slot
-        i = i + 1
-    end
+		-- Move onto the next slot
+		i = i + 1
+	end
 	
 	-- Add skill 'attribute_bonus'
 	hero:AddAbility('attribute_bonus')
 	
 	-- print all abilities
-	print('Hero:', hero:GetUnitName())
-	for index = 0, 16 do
-		if hero:GetAbilityByIndex(index) ~= nil then
-			abilityName = hero:GetAbilityByIndex(index):GetAbilityName()
-			print(abilityName)
+	--print('Hero:', hero:GetUnitName())
+	--for index = 0, 16 do
+	--	if hero:GetAbilityByIndex(index) ~= nil then
+	--		abilityName = hero:GetAbilityByIndex(index):GetAbilityName()
+	--		print(abilityName)
+	--	end
+	--end
+end
+
+function DotaPvP:ChangeHero(hero, newHeroName)
+	local playerID = hero:GetPlayerOwnerID()
+	local ply = PlayerResource:GetPlayer(playerID)
+	if not PlayerResource:IsValidPlayerID(playerID) then
+		print('Invalid playerID')
+		return nil
+	end
+	if ply then
+		-- Grab info
+		local exp = hero:GetCurrentXP()
+		local gold = hero:GetGold()
+
+		local slots = {}
+		for i=0, 11 do
+			local item = hero:GetItemInSlot(i)
+			if item then
+				-- Workout purchaser
+				local purchaser = -1
+				if item:GetPurchaser() ~= hero then
+					purchaser = item:GetPurchaser()
+				end
+
+				-- Store the item
+				slots[i] = {
+					purchaser = purchaser,
+					purchaseTime = item:GetPurchaseTime(),
+					currentCharges = item:GetCurrentCharges(),
+					StacksWithOtherOwners = item:StacksWithOtherOwners(),
+					sort = item:GetAbilityName()
+				}
+
+				-- Remove the item
+				hero:RemoveItem(item)
+			end
+		end
+
+		-- Replace the hero
+		local newHero = nil
+		print('playerID:', playerID)
+		print('newHeroName:', newHeroName)
+		print('gold:', gold)
+		print('exp:', exp)
+		newHero = PlayerResource:ReplaceHeroWith(playerID, newHeroName, gold, exp)
+
+		-- Validate new hero
+		if newHero then
+			local blockers = {}
+			-- Give items
+			for i=0, 11 do
+				local item = slots[i]
+				if item then
+					local p = (item.purchaser == -1 and newHero) or item.purchaser
+					local it = CreateItem(item.sort, p, p)
+					it:SetPurchaseTime(item.purchaseTime)
+					it:SetCurrentCharges(item.currentCharges)
+					it:SetStacksWithOtherOwners(item.StacksWithOtherOwners)
+					newHero:AddItem(it)
+				else
+					local it = CreateItem('item_blink', newHero, newHero)
+					newHero:AddItem(it)
+					table.insert(blockers, it)
+				end
+			end
+
+			-- Remove blocks
+			for k,v in pairs(blockers) do
+				-- Remove this blocker
+				newHero:RemoveItem(v)
+			end
+
+			-- Return their new hero
+			return newHero
 		end
 	end
 end
 
-function DotaPvP:ChangeHero(hero, newHeroName)
-    local playerID = hero:GetPlayerID()
-    local ply = PlayerResource:GetPlayer(playerID)
-    if ply then
-        -- Grab info
-        local exp = hero:GetCurrentXP()
-        local gold = hero:GetGold()
-
-        local slots = {}
-        for i=0, 11 do
-            local item = hero:GetItemInSlot(i)
-            if item then
-                -- Workout purchaser
-                local purchaser = -1
-                if item:GetPurchaser() ~= hero then
-                    purchaser = item:GetPurchaser()
-                end
-
-                -- Store the item
-                slots[i] = {
-                    purchaser = purchaser,
-                    purchaseTime = item:GetPurchaseTime(),
-                    currentCharges = item:GetCurrentCharges(),
-                    StacksWithOtherOwners = item:StacksWithOtherOwners(),
-                    sort = item:GetAbilityName()
-                }
-
-                -- Remove the item
-				hero:RemoveItem(item)
-            end
-        end
-
-        -- Replace the hero
-        local newHero = PlayerResource:ReplaceHeroWith(playerID, newHeroName, gold, exp)
-
-        -- Validate new hero
-        if newHero then			
-            local blockers = {}
-
-            -- Give items
-            for i=0, 11 do
-                local item = slots[i]
-                if item then
-                    local p = (item.purchaser == -1 and newHero) or item.purchaser
-                    local it = CreateItem(item.sort, p, p)
-                    it:SetPurchaseTime(item.purchaseTime)
-                    it:SetCurrentCharges(item.currentCharges)
-                    it:SetStacksWithOtherOwners(item.StacksWithOtherOwners)
-                    newHero:AddItem(it)
-                else
-                    local it = CreateItem('item_blink', newHero, newHero)
-                    newHero:AddItem(it)
-                    table.insert(blockers, it)
-                end
-            end
-
-            -- Remove blocks
-            for k,v in pairs(blockers) do
-                -- Remove this blocker
-                newHero:RemoveItem(v)
-            end
-
-            -- Return their new hero
-            return newHero
-        end
-    end
-end
-
 function DotaPvP:ChooseRandomHero()
-    return self.heroList[math.random(1, #self.heroList)]
+	return self.heroList[math.random(1, #self.heroList)]
 end
 
 function DotaPvP:GetRandomAbility(sort)
-    if not sort or not self.vAbListSort[sort] then
-        sort = 'Abs'
-    end
+	if not sort or not self.vAbListSort[sort] then
+		sort = 'Abs'
+	end
 
-    return self.vAbListSort[sort][math.random(1, #self.vAbListSort[sort])]
+	return self.vAbListSort[sort][math.random(1, #self.vAbListSort[sort])]
 end
 
 function DotaPvP:RemoveAllSkills(hero)
-    for index = 0, 16 do
+	for index = 0, 16 do
 		if hero:GetAbilityByIndex(index) ~= nil then
 			abilityName = hero:GetAbilityByIndex(index):GetAbilityName()
 			hero:RemoveAbility(abilityName)
@@ -523,7 +512,7 @@ end
 function DotaPvP:RefreshAllSkills(hero)
 	-- Refresh mana
 	hero:GiveMana(99999)
-    -- Refresh all skills
+	-- Refresh all skills
 	for index = 0, 16 do
 		if hero:GetAbilityByIndex(index) ~= nil then
 			hero:GetAbilityByIndex(index):EndCooldown()
